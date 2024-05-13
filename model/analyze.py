d7205033750901faf0d8086ef57bfa6ff4b09f03
timthedev07 <timpersonal07@gmail.com>
2024-05-13 18:34:22 +0200

    update

--- model/analyze.py
+++ model/analyze.py
@@ -11,6 +11,15 @@ def readCSV(fname="data.csv"):
     data = pd.read_csv(path.join(DATADIR, fname))
     return data
 
+
 def mse(y_true, y_pred):
     return np.mean((y_true - y_pred)**2)
 
+def videoToPeaks(fname: str):
+    pass
+
+def processCSV():
+    pass
+
+def processData():
+    pass
