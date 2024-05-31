e94510de64408bcfd3ed52e26506b34cceb98e2d
timthedev07 <timpersonal07@gmail.com>
2024-05-31 18:13:47 +0200

    update

--- model/analyze.py
+++ model/analyze.py
@@ -21,7 +21,8 @@ SCRIPT_FILE_DIR = path.dirname(path.realpath(__file__))
 DATADIR = path.join(SCRIPT_FILE_DIR, "../data/peaks")
 TRIAL_NUM = 5
 PEAK_IND = (2, 5)  # 7 not included, 0-indexed
-K = 50  # to be changed
+GRAPH_START = 9
+K = 100
 R = 2.5 / 100
 η = 0.85
 mError = 0.01 / 1000  # now in kg
@@ -40,32 +41,6 @@ def mse(y_true, y_pred):
 
 def expectedGradient():
     return 9 * (math.pi) ** 2 * R**2 * η**2 / K
-
-
-def gradientDelta(x, y):
-    N = len(x)
-    x_mean = np.mean(x)
-    y_mean = np.mean(y)
-    S_xx = np.sum((x - x_mean) ** 2)
-    S_xy = np.sum((x - x_mean) * (y - y_mean))
-    m = S_xy / S_xx
-    b = y_mean - m * x_mean
-
-    # Calculate residuals and variance
-    residuals = y - (m * x + b)
-    sigma_squared = np.sum(residuals**2) / (N - 2)
-
-    # Calculate uncertainty in slope (Delta m)
-    Delta_m = np.sqrt(sigma_squared / S_xx)
-
-    # Maximum and minimum slopes
-    m_max = m + Delta_m
-    m_min = m - Delta_m
-
-    # Corresponding y-intercepts
-    b_max = y_mean - m_max * x_mean
-    b_min = y_mean - m_min * x_mean
-    return ((m_max, b_max), (m_min, b_min))
 
 
 # Calculate the average damping ratio for each row in the dataframe
@@ -106,12 +81,16 @@ def avgDampingRatio(row: pd.Series, mode: Union["uncertainties", "vals"]):
                 4 * (math.pi) ** 2 + delta**2
             )  # Calculate the damping ratio using the logarithmic decrement
 
+            dzeta_ddelta = (
+                4 * math.pi**2 / math.pow(delta**2 + 4 * math.pi**2, 1.5)
+            )  # Calculate the derivative of zeta wrt delta
+
             errAbsZeta = (
-                zeta * 2 * (errAbsDelta / delta)
+                dzeta_ddelta * errAbsDelta
             )  # Calculate the absolute error of zeta
 
             errAbsZetaSq = (
-                2 * (zeta) * (errAbsZeta)
+                2 * zeta * errAbsZeta
             )  # Calculate the absolute error of the squared damping ratio
 
             ratios = np.append(
@@ -142,7 +121,7 @@ def averageData() -> pd.DataFrame:
     for i in range(PEAK_IND[0], PEAK_IND[1]):
         # convert from cm to m
         data[f"Peak {i - PEAK_IND[0] + 1}"] = data[f"Peak {i - PEAK_IND[0] + 1}"] / 100
-    return data
+    return data.round(3)
 
 
 def processData():
@@ -168,29 +147,82 @@ def processData():
     return df
 
 
-def main():
-    plt.figure()
-
+def setup(plt, endpoint, y):
     # config
     plt.xlabel(r"$m^{-1}$ (\si{\per\kilo\g})")
     plt.ylabel(r"$\zeta^2$ (unitless ratio)")
 
-    # prepare data
-    df = processData()
 
-    x = df["Mass Reciprocal"]
-    y = df["Zeta Squared"]
-    m, b = np.polyfit(x, y, 1)
-    endpoint = x.max()
-    x_1 = np.linspace(0, endpoint, 2)
-    y_1 = m * x_1 + b
+def fmt(x):
+    return (
+        np.format_float_scientific(np.float32(x), unique=False, precision=3)
+        .replace("e-0", "\\times 10^{-")
+        .replace("e-", "\\times 10^{-")
+        + "}"
+    )
 
-    r = np.corrcoef(x, y)
-    (mMax, bMax), (mMin, bMin) = gradientDelta(x, y)
-    yMax = mMax * x_1 + bMax
-    yMin = mMin * x_1 + bMin
 
-    # scatter plot
+def bestfit(plt, x, y, x_0, y_0, m, b):
+    style = {"color": "brown"}
+    r = np.corrcoef(x_0, y_0)
+    (bf,) = plt.plot(x, y, label=f"Best fit line")
+    t1 = plt.text(16.5, y_0[len(y_0) - 1] + 0.0012, f"$r = {r[0][1]:.3f}$", style)
+    t2 = plt.text(
+        15.5,
+        y_0[len(y_0) - 1],
+        f"$y = {fmt(m)}x + {b:.3f}$",
+        style,
+    )
+    t1.set_bbox(dict(facecolor="navajowhite", alpha=1, edgecolor="orange"))
+    t2.set_bbox(dict(facecolor="navajowhite", alpha=1, edgecolor="orange"))
+    return bf
+
+
+def theoretical(plt, endpoint):
+    m = expectedGradient()
+    style = {"color": "g"}
+    (th,) = plt.plot(
+        [GRAPH_START, endpoint],
+        [GRAPH_START * m, endpoint * m],
+        "g--",
+        label="Stoke's Law Prediction",
+    )
+    t = plt.text(16.5, 0.008, f"$y = {fmt(m)}x$", style)
+    t.set_bbox(dict(facecolor="palegreen", alpha=1, edgecolor="limegreen"))
+    return th
+
+
+def drawMaxMin(plt, x, y, errY):
+    n = len(x)
+    maxM = (y[n - 1] + errY[n - 1] - (y[0] - errY[0])) / (x[n - 1] - x[0])
+    minM = (y[n - 1] - errY[n - 1] - (y[0] + errY[0])) / (x[n - 1] - x[0])
+    pA_1 = (x[0], y[0] - errY[0])
+    pB_1 = (x[0], y[0] + errY[0])
+
+    maxB = pA_1[1] - maxM * pA_1[0]
+    minB = pB_1[1] - minM * pB_1[0]
+
+    xWithZero = np.insert(x, 0, GRAPH_START)
+
+    (minl,) = plt.plot(
+        xWithZero,
+        minM * xWithZero + minB,
+        label="Minimum slope",
+        color="mediumslateblue",
+        linestyle="dotted",
+    )
+    (maxl,) = plt.plot(
+        xWithZero,
+        maxM * xWithZero + maxB,
+        label="Maximum slope",
+        color="blueviolet",
+        linestyle="dotted",
+    )
+
+    return (minl, maxl)
+
+
+def scatter(plt, x, y, df):
     plt.scatter(x, y, c="#000000")
     plt.errorbar(
         x,
@@ -203,22 +235,47 @@ def main():
         ecolor="#000000",
     )
 
-    # # plot the line of best fit
-    (bf,) = plt.plot(x_1, y_1, label="Best fit line")
-    (minl,) = plt.plot(x_1, yMin, label="Minimum slope")
-    (maxl,) = plt.plot(x_1, yMax, label="Maximum slope")
+
+def main():
+    plt.figure(0)
+
+    # prepare data
+    df = processData()
+
+    x = df["Mass Reciprocal"]
+    y = df["Zeta Squared"]
+    m, b = np.polyfit(
+        x,
+        y,
+        1,
+    )
+    endpoint = x.max()
+    setup(plt, endpoint, y)
+    x_1 = np.linspace(GRAPH_START, endpoint, 2)
+    y_1 = m * x_1 + b
+
+    # scatter plot
+    scatter(plt, x, y, df)
+
+    # plot the line of best fit
+    bf = bestfit(plt, x_1, y_1, x, y, m, b)
 
     # plot the expected line
-    (th,) = plt.plot(
-        [0, endpoint],
-        [0, endpoint * expectedGradient()],
-        "--",
-        label="Stoke's Law Prediction",
-    )
+    th = theoretical(plt, endpoint)
 
-    plt.legend(handles=[bf, th, minl, maxl], frameon=False)
+    plt.legend(handles=[bf, th], frameon=False)
 
     plt.savefig(path.join(SCRIPT_FILE_DIR, "figures", "graph.png"), dpi=300)
+
+    # plot the max and min lines
+    plt.figure(1)
+    scatter(plt, x, y, df)
+    minl, maxl = drawMaxMin(plt, x, y, df["Zeta Squared Uncertainties"])
+    th = theoretical(plt, endpoint)
+    plt.legend(handles=[th, minl, maxl], frameon=False)
+
+    setup(plt, endpoint, y)
+    plt.savefig(path.join(SCRIPT_FILE_DIR, "figures", "graph_max_min.png"), dpi=300)
 
 
 if __name__ == "__main__":
---
+++ model/trial.py
@@ -0,0 +1,50 @@
+import numpy as np
+import math
+import pandas as pd
+from os import path
+
+SCRIPT_FILE_DIR = path.dirname(path.realpath(__file__))
+DATADIR = path.join(SCRIPT_FILE_DIR, "../data/peaks")
+r = 0.025
+masses = np.array([52.48, 60.94, 69.72, 86.51, 93.47]) / 1000
+η = 0.85
+A_0 = 0.05
+K = 100
+
+TRIAL_NUM = 5
+
+
+# Load the raw data
+def readCSV(fname="data.csv"):
+    data = pd.read_csv(path.join(DATADIR, fname))
+    return data
+
+
+def allData() -> pd.DataFrame:
+    return [readCSV(f"Trial {i}-表格 1.csv") for i in range(1, TRIAL_NUM + 1)]
+
+
+def hi():
+
+    for mass in masses:
+        ζ = math.sqrt((9 * (math.pi) ** 2 * r**2 * η**2) / (mass * K))
+        ratio = math.e ** ((2 * math.pi * ζ) / (math.sqrt(1 - ζ**2)))
+        peaks = [A_0 / (ratio) ** i for i in range(1, 4)]
+        print(np.array(peaks) * 100)
+
+
+def main():
+    dfs = allData()
+    s = ""
+    for i in range(dfs[0].shape[0]):
+        r = f"{dfs[0].iloc[i, 1]} "
+        for j in range(3):
+            for k in range(TRIAL_NUM):
+                r += f" & {dfs[k].iloc[i, 2 + j]}"
+        print(r + " \\\\ \\hline")
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
